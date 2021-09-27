499831a98a0fcb72929faa3e4f92884a3cbe6a7a
Davidson Bpe <davidsomixer@gmail.com>
2021-09-27 14:51:56 -0300

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
   from http.server import SimpleHTTPRequestHandler as Handler
   from http.server import HTTPServer as Server
-PORT = int(os.getenv('PORT', 3000))
+PORT = int(os.getenv('PORT', 3100))
 os.chdir('static')
 
 httpd = Server(("", PORT), Handler)
